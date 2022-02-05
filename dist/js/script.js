4c3acfa71479581570637e33ca06790402b2b878
MishaPeshin <michael.peshon@gmail.com>
2022-02-05 17:39:27 +0500

    Добавлена адаптивность

--- dist/js/script.js
+++ dist/js/script.js
@@ -98,55 +98,59 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   //Accordion 
-  // const arts = document.querySelectorAll('.portfolio__item'),
-  //       button = document.querySelector('.portfolio__button');
-  // function hideArts() {
-  //     arts.forEach((item, i) => {
-  //         if (i > 3) {
-  //             item.classList.add('hide');
-  //             button.textContent = 'Load more';
-  //         }
-  //     });
-  // }
-  // function showArts() {
-  //     arts.forEach(item =>{
-  //         if (item.classList.contains('hide')) {
-  //             item.classList.remove('hide');
-  //             button.textContent = 'Hide works';
-  //         }
-  //     });
-  // }
-  // button.addEventListener('click', (e) => {
-  //     if (e.target.innerText == 'Load more'){
-  //         showArts();
-  //     } else {
-  //         hideArts();
-  //     }
-  // });
-  // arts.forEach(item =>{
-  //     item.addEventListener('mouseenter', (e) =>{
-  //     });
-  // });
-  // hideArts();
-  //     //Cards events 
-  // const cardsDescr = document.querySelectorAll('.portfolio__descr');
-  // function hideCards(card) {
-  //     card.forEach(item => {
-  //         item.classList.add('hide');
-  //     });
-  // }
-  // hideCards(cardsDescr);
-  // arts.forEach((item, i) => {
-  //     item.addEventListener('mouseover', () => {
-  //         cardsDescr[i].classList.remove('hide');
-  //         cardsDescr[i].classList.add('hovered');
-  //     });
-  //     item.addEventListener('mouseout', () => {
-  //         cardsDescr[i].classList.add('hide');
-  //         cardsDescr[i].classList.remove('hovered');
-  //     });
-  // });
-  // hamburger
+  const arts = document.querySelectorAll('.portfolio__item'),
+        button = document.querySelector('.portfolio__button');
+
+  function hideArts() {
+    arts.forEach((item, i) => {
+      if (i > 3) {
+        item.classList.add('hide');
+        button.textContent = 'Load more';
+      }
+    });
+  }
+
+  function showArts() {
+    arts.forEach(item => {
+      if (item.classList.contains('hide')) {
+        item.classList.remove('hide');
+        button.textContent = 'Hide works';
+      }
+    });
+  }
+
+  button.addEventListener('click', e => {
+    if (e.target.innerText == 'Load more') {
+      showArts();
+    } else {
+      hideArts();
+    }
+  });
+  arts.forEach(item => {
+    item.addEventListener('mouseenter', e => {});
+  });
+  hideArts(); //Cards events 
+
+  const cardsDescr = document.querySelectorAll('.portfolio__descr');
+
+  function hideCards(card) {
+    card.forEach(item => {
+      item.classList.add('hide');
+    });
+  }
+
+  hideCards(cardsDescr);
+  arts.forEach((item, i) => {
+    item.addEventListener('mouseover', () => {
+      cardsDescr[i].classList.remove('hide');
+      cardsDescr[i].classList.add('hovered');
+    });
+    item.addEventListener('mouseout', () => {
+      cardsDescr[i].classList.add('hide');
+      cardsDescr[i].classList.remove('hovered');
+    });
+  }); // hamburger
+
   const hamburger = document.querySelector('.navigation__hamburger'),
         menu = document.querySelector('.navigation__wrapper');
   console.log(hamburger);
--- src/js/main.js
+++ src/js/main.js
@@ -4,65 +4,65 @@ document.addEventListener('DOMContentLoaded', () =>{
 
         //Accordion 
 
-    // const arts = document.querySelectorAll('.portfolio__item'),
-    //       button = document.querySelector('.portfolio__button');
+    const arts = document.querySelectorAll('.portfolio__item'),
+          button = document.querySelector('.portfolio__button');
 
-    // function hideArts() {
-    //     arts.forEach((item, i) => {
-    //         if (i > 3) {
-    //             item.classList.add('hide');
-    //             button.textContent = 'Load more';
-    //         }
-    //     });
-    // }
+    function hideArts() {
+        arts.forEach((item, i) => {
+            if (i > 3) {
+                item.classList.add('hide');
+                button.textContent = 'Load more';
+            }
+        });
+    }
 
-    // function showArts() {
-    //     arts.forEach(item =>{
-    //         if (item.classList.contains('hide')) {
-    //             item.classList.remove('hide');
-    //             button.textContent = 'Hide works';
-    //         }
-    //     });
-    // }
+    function showArts() {
+        arts.forEach(item =>{
+            if (item.classList.contains('hide')) {
+                item.classList.remove('hide');
+                button.textContent = 'Hide works';
+            }
+        });
+    }
 
-    // button.addEventListener('click', (e) => {
-    //     if (e.target.innerText == 'Load more'){
-    //         showArts();
-    //     } else {
-    //         hideArts();
-    //     }
-    // });
+    button.addEventListener('click', (e) => {
+        if (e.target.innerText == 'Load more'){
+            showArts();
+        } else {
+            hideArts();
+        }
+    });
 
-    // arts.forEach(item =>{
-    //     item.addEventListener('mouseenter', (e) =>{
-    //     });
-    // });
+    arts.forEach(item =>{
+        item.addEventListener('mouseenter', (e) =>{
+        });
+    });
 
-    // hideArts();
+    hideArts();
     
-    //     //Cards events 
+        //Cards events 
 
-    // const cardsDescr = document.querySelectorAll('.portfolio__descr');
+    const cardsDescr = document.querySelectorAll('.portfolio__descr');
 
-    // function hideCards(card) {
-    //     card.forEach(item => {
-    //         item.classList.add('hide');
-    //     });
-    // }
+    function hideCards(card) {
+        card.forEach(item => {
+            item.classList.add('hide');
+        });
+    }
 
-    // hideCards(cardsDescr);
+    hideCards(cardsDescr);
 
-    // arts.forEach((item, i) => {
-    //     item.addEventListener('mouseover', () => {
-    //         cardsDescr[i].classList.remove('hide');
-    //         cardsDescr[i].classList.add('hovered');
-    //     });
+    arts.forEach((item, i) => {
+        item.addEventListener('mouseover', () => {
+            cardsDescr[i].classList.remove('hide');
+            cardsDescr[i].classList.add('hovered');
+        });
 
-    //     item.addEventListener('mouseout', () => {
-    //         cardsDescr[i].classList.add('hide');
-    //         cardsDescr[i].classList.remove('hovered');
-    //     });
-    // });
+        item.addEventListener('mouseout', () => {
+            cardsDescr[i].classList.add('hide');
+            cardsDescr[i].classList.remove('hovered');
+        });
+    });
 
         // hamburger
 
